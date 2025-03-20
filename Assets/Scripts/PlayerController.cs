03660f3aa249833eb18a09426d8e896975fb1866
typpsii <kaedynhindley@gmail.com>
2025-03-20 08:26:42 -0700

    Player can fire bullets. Skeleton for shot type created

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,33 +5,45 @@ using UnityEngine;
 public class PlayerController : SingletonMonoBehavior<PlayerController>
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private shotType equipped;
+    enum shotType {SIMPLE, TRIPLE};
 
     private Rigidbody2D rb;
+    public GameObject simpleBullet;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        equipped = shotType.SIMPLE;
     }
 
     private void OnEnable()
     {
         InputManager.Instance.OnMove.AddListener(MovePlayer);
+        InputManager.Instance.OnFire.AddListener(Fire);
     }
 
     private void OnDisable()
     {
         InputManager.Instance.OnMove.RemoveListener(MovePlayer);
+        InputManager.Instance.OnFire.RemoveListener(Fire);
     }
 
-    private void OnFire()
+    private void Fire()
     {
-        
+        Debug.Log("Fire!");
+        // Checks what is equipped
+        switch (equipped)
+        {
+        case shotType.SIMPLE:
+            Instantiate(simpleBullet, transform.position, transform.rotation);
+            break;
+        }
     }
 
     private void MovePlayer(Vector2 moveDirection)
     {
         rb.linearVelocity = moveDirection * moveSpeed;
-        Debug.Log("Moving!!!!");
     }
 
 
