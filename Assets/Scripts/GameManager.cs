a3aa028834cb5b0cb22c09238014fa8a45d6e750
Ariana Rice <69221845+ariceeee@users.noreply.github.com>
2025-03-27 18:05:07 -0700

    SceneHandler transitions between existing levels and ready to handle Level_3, GameOver, and Win once those get added)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
@@ -10,14 +13,44 @@ public class GameManager : SingletonMonoBehavior<GameManager>
     private int score = 0;
     private int lives = 3;
 
+    [SerializeField] private int currNumEnemies;
+
+    // start gets called when new level loaded
     void Start()
     {
-        hudPanel.ResetHUD();
+        hudPanel.ResetHUD(); // note this resets score/lives @ start of every level, do we want this to happen???
+        currNumEnemies = CountNumEnemiesInScene();
     }
 
     public void UpdateScore()
     {
         score++;
         hudPanel.UpdateScore(score);
+    }
+
+    public void UpdateEnemyCount()
+    {
+        currNumEnemies--;
+
+        if (isLevelCompleted())
+        {
+            SceneHandler.Instance.NextLevel();
+        }
+    }
+
+    private bool isLevelCompleted()
+    {
+        if (currNumEnemies == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private int CountNumEnemiesInScene()
+    {
+        List<GameObject> enemies = new List<GameObject>();
+        enemies.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
+        return enemies.Count;
     }
 }
